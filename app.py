00e9da2c33ab4e5158278e47c2f5ecd0621e8c1d
ykr-learning <>
2022-06-09 12:21:18 +0200

    refactoring lambda, fix http status on post when exists

--- app.py
+++ app.py
@@ -19,38 +19,36 @@ class Item(Resource):
         if next(filter(lambda x:x["name"] == name, items), None ) is not None:
             return {
                 "message": "Item with name {name} is already present".format(name=name)
-            }
+            }, 409
         item = {'name': name, 'price': data["price"]}
         items.append(item)
         return item, 201
     
     def delete(self, name):
-        if len(items) == 0:
+        item = next(filter(lambda x:x["name"] == name, items), None )
+        if item is None:
             return {"item": None}, 404
-        for item in items:
-            if item['name'] == name:
-                items.remove(item)
-                return {'message': 'item removed'}, 204
-        return {"item": None}, 404
+        else:
+            items.remove(item)
+            return {'message': 'item removed'}, 204
 
     def put(self, name):
         data = request.get_json()
-        if len(items) == 0:
-            item = {'name': name, 'price': 12.00}
+
+        item = next(filter(lambda x:x["name"] == name, items), None )
+        if item is None:
+            item = {"name": name, "price": data["price"]}
             items.append(item)
             return item, 201
-        for item in items:
-            if item["name"] == name:
-                if data["price"] != item["price"]:
-                    item["price"] = data["price"]
-                    return {"message": "Price updated to " + str(data["price"])}, 204
-                else:
-                    return {
-                        "message": "Price is already at " + str(data["price"])
-                    }, 200
-        item = {"name": name, "price": data["price"]}
-        items.append(item)
-        return item, 201
+        else:
+            if data["price"] != item["price"]:
+                item["price"] = data["price"]
+                return {"message": "Price updated to " + str(data["price"])}, 204
+            else:
+                return {
+                    "message": "Price is already at " + str(data["price"])
+                }, 200
+
 
 class ItemList(Resource):
     def get(self):
